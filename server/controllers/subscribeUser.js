19fa959cf17677ae40aa9321282cfbc1aa51caef
maainul <mainul080@yahoo.com>
2023-12-18 21:33:25 +0600

    User Email Verification and Verify User

--- server/controllers/subscribeUser.js
+++ server/controllers/subscribeUser.js
@@ -2,6 +2,10 @@ const MValidator = require("../validator/MValidator")
 const SubscriptionModel = require('../models/SubscribeUser')
 const validationLog = require("../utils/validationLog")
 const save = require("../utils/saveUtils")
+const { sendMail } = require("../utils/sendVerificationEmail")
+const generateVerificationCode = require("../utils/getVerificationCode")
+const findOne = require("../utils/findOne")
+const SubscribeUserModel = require("../models/SubscribeUser")
 
 const validationRules = {
     name: {
@@ -34,18 +38,32 @@ const subscribeUser = async (req, res) => {
                 errors: validationResult.errors
             })
         }
-        // TODO : Send Confirmation Mail to User Mail.Then Verify User after Verify Subscription Will be possible
-        const subsUser = await save(SubscriptionModel, {
-            name: name,
-            email: email,
-        })
-        console.log(`User Subscription Added Successfully :\n ${subsUser}`)
-        return res.status(201).send({
-            success: true,
-            message: 'User Subscription Successful',
-            data: subsUser
-        })
 
+        // generate Code 
+        const verificationCode = generateVerificationCode();
+
+        const result = await sendMail(email, verificationCode)
+        if (result.success) {
+            const subsUser = await save(SubscriptionModel, {
+                name: name,
+                email: email,
+                verificationCode: verificationCode
+            })
+
+            console.log(`User Subscription Added Successfully :\n ${subsUser}`)
+            return res.status(201).send({
+                success: true,
+                message: 'User Subscription Successful',
+                data: subsUser
+            })
+        } else {
+            console.log(`Email Send Failed :\n ${result.data}`)
+            return res.status(400).send({
+                success: result.success,
+                data: result.data,
+                message: result.message
+            })
+        }
     } catch (error) {
         console.error('Error In User Subscription API:', error)
         const status = error.status || 500
@@ -115,4 +133,43 @@ const UserInfo = [
     }
 ]
 
-module.exports = { subscribeUser, unSubscribeUser }
+
+
+const verifyUser = async (req, res) => {
+    try {
+        const { email, name, verificationCode } = req.body
+
+        const user = await UserInfo.find(user => user.email === email)
+        if (user.verificationCode === verificationCode) {
+            const saveUserInfo = await save(SubscriptionModel, {
+                name: user?.name,
+                email: user?.email,
+                subscribeDate: user?.date, // Set to the current date and time
+                subscriptionFor: user?.subscriptionFor,
+                subscriptionStatus: true,
+            })
+            return res.status(201).send({
+                success: true,
+                data: saveUserInfo,
+                message: "Verfication Successful"
+            })
+        } else {
+            return res.status(400).send({
+                success: result.success,
+                data: result.data,
+                message: "Email Verification Failed"
+            })
+        }
+    } catch (error) {
+        console.error('Error In Veriry API:', error)
+        const status = error.status || 500
+        return res.status(status).send({
+            success: false,
+            message: 'Error In Veriry API',
+            error: error.message || error,
+        })
+    }
+}
+
+
+module.exports = { subscribeUser, unSubscribeUser, verifyUser }
