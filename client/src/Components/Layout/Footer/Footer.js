197fd7c2571675023bd0a3aecba6ea3c5e209de3
maainul <mainul080@yahoo.com>
2024-01-03 17:29:30 +0600

    Layout

--- client/src/Components/Layout/Footer/Footer.js
+++ client/src/Components/Layout/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Footer.css'
+import '../Footer/Footer.css'
 
 const Footer = () => {
     return (
-        <div className='footer' >Footer</div>
+        <div className='footer'>Footer</div>
     )
 }
 
--- client/src/Components/Layout/MainLayout/MainLayout.js
+++ client/src/Components/Layout/MainLayout/MainLayout.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './MainLayout.css'
 import { Toaster } from 'react-hot-toast';
-
+import Footer from '../Footer/Footer'
 const MainLayout = ({ children }) => {
     return (
-        <div className='container'>
-            <Toaster />
-            {children}
-        </div>
+        <>
+            <div className='container'>
+                <div className='sidebar'>Sidebar</div>
+                <div className='main-container'>
+                    <div className='header'>Header</div>
+                    <div className='main-container'>Container</div>
+                    <div className='footer'>Footer</div>
+                </div>
+                {/* <Footer />
+                <div>
+                    <Toaster />
+                    {children}
+                </div>
+                <Footer /> */}
+            </div>
+        </>
     )
 }
 
