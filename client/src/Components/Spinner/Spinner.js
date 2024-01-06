8e6a5980e900161515e00e25b8b4e8cc830fd901
maainul <mainul080@yahoo.com>
2024-01-06 22:06:52 +0600

    fix redirect issue

--- client/src/Components/Spinner/Spinner.js
+++ client/src/Components/Spinner/Spinner.js
@@ -1,29 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 const Spinner = () => {
-    // const [count, setCount] = useState(1);
+    const [count, setCount] = useState(3);
     const navigate = useNavigate();
     const location = useLocation();
-    // console.log(`Counter Value : ${count}`)
+    console.log(`Counter Value : ${count}`)
     console.log(`Navigate Information  : ${JSON.stringify(navigate, null, 4)}`)
     console.log(`Location information  : ${JSON.stringify(location, null, 4)}`)
 
     useEffect(() => {
-        // const interval = setInterval(() => {
-        // setCount((prevValue) => --prevValue);
-        // }, 1000);
-        // count === 1 &&
-        navigate("/signin")
-        // {
-        // state: location.pathname,
-        // });
-        // return () => clearInterval(interval);
-        // }, [count, navigate, location]);
-    }, [navigate, location]);
+        const interval = setInterval(() => {
+            setCount((prevValue) => --prevValue);
+        }, 1000);
+        count === 0 &&
+            navigate("/signin", {
+                state: location.pathname,
+            });
+        return () => clearInterval(interval);
+    }, [count, navigate, location]);
     return (
         <>
             <div style={{ height: "100vh" }} >
-                {/* <h1 className="Text-center">redirecting to you in {count} second </h1> */}
+                <h1 className="Text-center">redirecting to you in {count} second </h1>
                 <div>
                     <span>Loading...</span>
                 </div>
