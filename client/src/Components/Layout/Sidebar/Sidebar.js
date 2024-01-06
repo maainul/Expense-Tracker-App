7344ddaab228dccef8ddfae94cb8cc352b5db8b8
maainul <mainul080@yahoo.com>
2024-01-06 07:55:03 +0600

    Add dashboard to sidebar

--- client/src/Components/Layout/Sidebar/Sidebar.js
+++ client/src/Components/Layout/Sidebar/Sidebar.js
@@ -10,42 +10,42 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="brand-logo-text sidebarlogo">
-                <Link to={'/'}>
+                <Link to={'/dashboard'}>
                     <BrandLogo />
                 </Link>
-                <Link to={'/'} style={{ textDecoration: 'none' }} >
+                <Link to={'/dashboard'} style={{ textDecoration: 'none' }} >
                     <BrandTitle />
                 </Link>
             </div>
             <div className='menu'>
                 <div className='menuItem'>
                     <i className='menu-icon tf-icons bx bx-home-circle'></i>
-                    <Link to={'/'} className='menu-link'>Home</Link>
+                    <Link to={'/dashboard'} className='menu-link'>Home</Link>
                 </div>
 
                 <div className='menuItem'>
                     <i className='menu-icon tf-icons bx bx-store'></i>
-                    <Link to={'/expense'} className='menu-link'>Expense</Link>
+                    <Link to={'/dashboard/expense'} className='menu-link'>Expense</Link>
                 </div>
                 <div className='menuItem'>
                     <i class="menu-icon tf-icons bx bx-chart"></i>
-                    <Link to={'/analytics'} className='menu-link'>Analytics</Link>
+                    <Link to={'/dashboard/analytics'} className='menu-link'>Analytics</Link>
                 </div>
                 <div className='menuItem'>
                     <i class="menu-icon tf-icons bx bx-user"></i>
-                    <Link to={'/profile'} className='menu-link'>Profile</Link>
+                    <Link to={'/dashboard/profile'} className='menu-link'>Profile</Link>
                 </div>
                 {/* <Link to={'/about'}>About</Link> */}
                 {/* <Link to={'/user-list'}>Accounts</Link> */}
                 <div className='menu-last '>
                     <div className='menuItem'>
                         <i class="menu-icon tf-icons bx bx-support"></i>
-                        <Link to={'/analytics'} className='menu-link'>Supports</Link>
+                        <Link to={'/dashboard/analytics'} className='menu-link'>Supports</Link>
                         <span></span>
                     </div>
                     <div className='menuItem'>
                         <i class="menu-icon tf-icons bx bx-support"></i>
-                        <Link to={'/analytics'} className='menu-link'>Documentation</Link>
+                        <Link to={'/dashboard/analytics'} className='menu-link'>Documentation</Link>
                     </div>
                 </div>
             </div>
