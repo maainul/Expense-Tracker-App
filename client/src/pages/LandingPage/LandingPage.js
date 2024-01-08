a88929f7bc7b3ad7236101e7fbbf50021604a2d0
maainul <mainul080@yahoo.com>
2024-01-09 00:45:09 +0600

    Landing Page Design

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -7,6 +7,10 @@ import Twitter from '../../Components/SocialIcons/Twitter';
 import Gmail from '../../Components/SocialIcons/Gmail';
 import Github from '../../Components/SocialIcons/Github';
 import InboxImage from '../../img/get-update-inbox.png';
+import Port1 from '../../img/Port1.png';
+import Port2 from '../../img/prot2.png';
+import Port3 from '../../img/port3.png';
+import Port4 from '../../img/port4.jpg';
 const LandingPage = () => {
     return (
         <>
@@ -14,7 +18,7 @@ const LandingPage = () => {
                 <div className="landing-container">
                     <div className='less-width'>
                         <div className='nav-and-title'>
-                            <div className='brand-&-Title'>
+                            <div>
                                 <BrandLogo />
                                 <BrandTitle />
                             </div>
@@ -28,20 +32,33 @@ const LandingPage = () => {
                                 </div>
                             </div>
                         </div>
-
                         <div className='hero'>
                             <div className='left-hero-container'>
                                 <div>
                                     <p className='text-hero-primary'>Get Started</p>
-                                    <h1 className='text-hero-heading-text'>Your fitness advisor<br />
+                                    <h1 className='text-hero-heading-text'>Your <span style={{ color: "#FF7272" }}>EXPENSE</span> advisor<br />
                                         AIl in one place</h1>
                                     <p className='text-hero-secondary'>
-                                        When, while lovely valley teems with vapour around meand<br />
-                                        meridian sun strikes the upper impenetroble.
+                                        Get in, It will track Your Expense Automatically<br />
+                                        Simply Add expenses then see what wonder is comming.
                                     </p>
                                     <div className='landing-page-btn'>
-                                        <button className='btnland btn-clr-signin'>Signin</button>
-                                        <button className='btnland btn-clr-signup' >Signup</button>
+                                        <Link to={"/signin"} style={{
+                                            textDecoration: 'none',
+                                            color: "#FFF"
+                                        }}>
+                                            <button className='btnland btn-clr-signin'>
+                                                Signin
+                                            </button>
+                                        </Link>
+                                        <Link to={"/signup"} style={{
+                                            textDecoration: 'none',
+                                            color: "#FFF"
+                                        }}>
+                                            <button className='btnland btn-clr-signup' >
+                                                Signup
+                                            </button>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
@@ -50,31 +67,69 @@ const LandingPage = () => {
                                 2
                             </div>
                         </div>
+
+                    </div>
+                </div>
+                <div className='app-features'>
+                    <div className='app-features-container'>
+                        <p className='text-heading'>Features</p>
+                        <h2 className='app-features-secondary-heading'>Pages of Websites Works</h2>
+                        <div class="grid-container">
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                            <div class="grid-item">
+                                <img src={Port4} alt='hi' className='image-size' />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='get-latest-update'>
-                    <div className='image-and-email-box'>
-                        {/* <img src={InboxImage} alt='user' /> */}
-                    </div>
-                    <div className='get-latest-update-input-box'>
-                        <div className='latest-input-header'>
-                            <p className='latest-input-text'>Join Our Community</p>
-                            <h1>
-                                <span className='update-inbox-text'>Get latest app updates<br /> right at your inbox</span>
-                            </h1>
+                    <div className='get-latest-container'>
+                        <div className='get-latest-img-input-box'>
+                            <img src={InboxImage} alt="Get Latest Inbox" className='inbox-image' />
+                            <div className='get-latest-app-input-box'>
+                                <div className='latest-input-box'>
+                                    <p className='join-community'>Join our community</p>
+                                    <h1 className='latest-app-header'>Get latest app updates<br />
+                                        right at your inbox</h1>
+
+                                    <div className='subscribe-form-container'>
+                                        <form>
+                                            <label>
+                                                <input className='subs-input-box' type="text" placeholder="Enter Your Email Id..." />
+                                                <button className='subsbutton'>Subscribe</button>
+                                            </label>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className='get-latest-app-input-box'>
-                            <form>
-                                <label>
-                                    <input type="text" placeholder="Enter Your Email Id..." />
-                                    <button>Subscribe</button>
-                                </label>
-                            </form>
-
-
+                    </div>
+                </div>
+                <div className='app-available'>
+                    <div className='app-availabe-container'>
+                        <h1 className='section-header'>App Is Available For Free On<br /> Google Play & App Store</h1>
+                        <p className='section-secondary-text'>We'll help you achieve your marketing & business goals</p>
+                        <div className='btn-grp-center'>
+                            <div className='landing-page-btn-footer'>
+                                <button className='btnland btn-clr-signin appstorecolor'>Apple Store</button>
+                                <button className='btnland btn-clr-signup playstore' >Google Play</button>
+                            </div>
                         </div>
                     </div>
-
                 </div>
                 <footer className='footer'>
                     <div className='brand-logo-title'>
@@ -99,7 +154,8 @@ const LandingPage = () => {
                     </div>
                     <p className='footer-Copyright'>© 2024 Designed and Developed By <strong>ButterflyTech</strong>. All Rights Reserved</p>
                 </footer>
-            </div>
+
+            </div >
         </>
     )
 }
