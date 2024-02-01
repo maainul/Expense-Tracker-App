6e63609bf54f9ae2e32cfa8734394f0c11812df2
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-02-01 13:22:44 +0600

    Add Expense Type Pagination

--- server/controllers/expenseType/getAllExpTypeCtrl.js
+++ server/controllers/expenseType/getAllExpTypeCtrl.js
@@ -1,46 +1,14 @@
-import { logger } from "../../middleware/logMiddleware.js";
-import ExpenseType from "../../models/ExpenseType.js";
 import { serv } from './../../service/services.js';
-
-
-
 
 export const getAllExpTypeCtrl = async (req, res) => {
     try {
-        // data destructure from query string like ?key=value
-        const { search } = req.query
-        // condition for searching
-        const queryObject = {}
-        //    const queryResult = ExpenseType.find(queryObject)
-        //   const expeTyps = queryResult
-
-        if (search) {
-            queryObject.name = { $regex: search, $options: 'i' }
-        }
-
-        console.log("################################")
-        console.log(queryObject)
-        console.log("################################")
-
-        const queryResult = await ExpenseType.find(queryObject)
-        console.log("################# Expense Type ###########################")
-        console.log(queryResult)
-        console.log("################# Expense Type ###########################")
-
-        // Set default sort order if not provided or unexpected
-        // const expeTyps = await serv.expenseTypeService.getAllExpTypeServ({ req })
-        if (!queryResult) {
-            return res.status(200).send({
-                success: true,
-                message: 'No categories Found'
-            })
-        }
-        logger.info(`Expense Type data ==> \n ${queryResult}`)
+        const expeTyps = await serv.expenseTypeService.getAllExpTypeServ({ req })
         return res.status(200).send({
             success: true,
+            expTypeCount : expeTyps.totalExpTyps,
             message: 'Get all expense Type successfully',
-            totalExpTypes: queryResult.length,
-            data: queryResult
+            numOfPage: expeTyps.numOfPage,
+            data: expeTyps.expTyps
         });
     } catch (error) {
         console.error('Error In Get Expense Type API:', error);
--- server/service/expenseType/getAllExpTypeServ.js
+++ server/service/expenseType/getAllExpTypeServ.js
@@ -1,25 +1,54 @@
+import { logger } from "../../middleware/logMiddleware.js";
 import ExpenseTypeModel from "../../models/ExpenseType.js"
 
 
 export const getAllExpTypeServ = async ({ req }) => {
-    const { sortOrder } = req.body;
-    let queryResult = await ExpenseTypeModel.find()
-        .sort(
-            {
-                createdAt: sortOrder === "asc" ? 1 : -1
-            }
-        );
-    //paginaiton
-    // const page = Number(req.query.page) || 1
-    // const limit = Number(req.query.limit) || 10
-    // const skip = (page - 1) * limit
-    // queryResult = queryResult.skip(skip).limit(limit)
-    // count 
-    //  const totalExpTyp = await ExpenseTypeModel.countDocuments(queryResult)
-    //   const numOfPage = Math.ceil(totalExpTyp / limit)
+    // Query Parameter for Search
+    const {search, sort } = req.query;
 
-    //   const expTyps = await queryResult
+    //conditons for searching filters
+    let queryObject = {};
 
-    return queryResult
+    // Check Search from query
+    if (search) {
+        queryObject.name = { $regex: search, $options: "i" };
+    }
+
+    // Build the Mongoose query
+    let queryResult = ExpenseTypeModel.find(queryObject);
+
+    // Sorting
+    if (sort === "latest") {
+        queryResult = queryResult.sort("-createdAt");
+    }
+    if (sort === "oldest") {
+        queryResult = queryResult.sort("createdAt");
+    }
+    if (sort === "a-z") {
+        queryResult = queryResult.sort("name");
+    }
+    if (sort === "z-a") {
+        queryResult = queryResult.sort("-name");
+    }
+    //pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    queryResult = queryResult.skip(skip).limit(limit);
+
+    // Expense-type count
+    const totalExpTyps = await ExpenseTypeModel.countDocuments(queryResult);
+    const totlExpTyp = await ExpenseTypeModel.countDocuments(queryObject);
+
+    const numOfPage = Math.ceil(totlExpTyp / limit);
+    // Execute Query
+    const expTyps = await queryResult;
+    logger.info(`Expense Type data From Expense Type Service==> \n ${expTyps}`);
+    return {
+        totalExpTyps,
+        expTyps,
+        numOfPage
+    }
 }
 
